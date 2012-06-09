ee5825ba44029c6b378778e31f55c846524b0e5b
Duncan McGreggor <duncan@dreamhost.com>
2012-06-09 00:56:16 -0400

    Added a custom exceptions module.

---
+++ akanda/exceptions.py
@@ -0,0 +1,20 @@
+class Error(Exception):
+    """
+    A base class for exceptions.
+    """
+    def __init__(self, msg=None):
+        if msg == None:
+            msg = self.__doc__.strip()
+        super(Error, self).__init__(msg)
+
+
+class PermissionDenied(Error):
+    """
+    You must be root to make changes to pf.
+    """
+
+
+class UnsupportedIOCTL(Error):
+    """
+    Is the pf device correct?
+    """
