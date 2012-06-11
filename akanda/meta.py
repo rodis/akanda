c93c4db6056eff86c6d21222988c30fae1f7900a
Duncan McGreggor <duncan@dreamhost.com>
2012-06-11 15:08:27 -0400

    Added setup stuff.

---
+++ akanda/meta.py
@@ -0,0 +1,11 @@
+display_name = "Akanda"
+library_name = "akanda"
+version = "0.1"
+author = "Duncan McGreggor"
+author_email = "dev-community@dreamhost.com"
+license = "BSD"
+url = "http://github.com/dreamhost/akanda"
+description = "Akanda Appliance"
+long_description = "A packet filter appliance for OpenStack deployments."
+requires = [
+    ]
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+from akanda import meta
+
+
+setup(
+    name=meta.display_name,
+    version=meta.version,
+    description=meta.description,
+    long_description=meta.long_description,
+    author=meta.author,
+    author_email=meta.author_email,
+    url=meta.url,
+    license=meta.license,
+    packages=find_packages(),
+    install_requires=meta.requires,
+    zip_safe=False
+    )
