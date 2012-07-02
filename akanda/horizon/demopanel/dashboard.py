9dbe4c61b2e2d997535dd92607f361423da52ede
Duncan McGreggor <duncan@dreamhost.com>
2012-07-02 14:39:15 -0400

    More tweaking of the demo dashboard.

--- akanda/horizon/demopanel/dashboard.py
+++ akanda/horizon/demopanel/dashboard.py
@@ -3,10 +3,16 @@ from django.utils.translation import ugettext_lazy as _
 import horizon
 
 
-class DemoPanel(horizon.Dashboard):
+class DemoPanelGroup(horizon.PanelGroup):
     name = _("Demo Panel")
     slug = "demopanel"
-    panels = ("overview", "services", "projects",)
+    panels = ("ovewview", "aliases", "nat", "portforward", "firewall",)
+
+
+class DemoPanel(horizon.Dashboard):
+    name = _("Admin")
+    slug = "demopanel"
+    panels = (DemoPanelGroup,)
     default_panel = "overview"
     roles = ("admin",)
 
