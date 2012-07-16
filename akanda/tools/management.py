7b71134122d247ecc05f4268d86cd1cb081c3dec
Mark McClain <mark.mcclain@dreamhost.com>
2012-07-15 22:43:22 -0400

    make configure script idempotent

---
+++ akanda/tools/management.py
@@ -0,0 +1,23 @@
+import sys
+
+from akanda.drivers import ifconfig
+
+def configure_management_interface():
+    mgr = ifconfig.InterfaceManager()
+
+    interfaces = mgr.get_interfaces(['em', 're'])
+    interfaces.sort(key=lambda x:x.ifname)
+    primary = interfaces[0]
+
+    if not primary.is_up:
+        mgr.up(primary)
+        primary = mgr.get_interface(primary)
+
+    for address in primary.addresses:
+        if str(address.ip).startswith('fe80'):
+            sys.stdout.write('%s%%%s' % (address.ip, primary.ifname))
+            sys.stdout.flush()
+            sys.exit()
+
+    sys.stderr.write('Unable to bring up first interface (%s)!\n' %
+                     primary.ifname)
