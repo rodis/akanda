55c8e2326e1a7c59d887e0d5d232f75c67d17733
Mark McClain <mark.mcclain@dreamhost.com>
2012-07-15 21:26:18 -0400

    initial skelton of temp pf manager

--- akanda/models.py
+++ akanda/models.py
@@ -60,18 +60,22 @@ class Interface(object):
 
 
 class FilterRule(object):
-    def __init__(self, action=None, interface=None, family=None, protocol=None,
-                 from_cidr=None, from_port=None, to_cidr=None, to_port=None,
+    def __init__(self, action=None, interface=None, family=None,
+                 protocol=None, source=None, source_port=None,
+                 destination_interface=None,
+                 destination=None, destination_port=None,
                  redirect=None, redirect_port=None):
 
         self.action = action
         self.interface = interface
         self.family = family
         self.protocol = protocol
-        self.from_cidr = from_cidr
-        self.from_port = from_port
-        self.to_cidr = to_cidr
-        self.to_port = to_port
+        self.from_table = from_table
+        self.source = source
+        self.source_port = source_port
+        self.destination_interface = destination_interface
+        self.destination = destination
+        self.desination_port = destination_port
         self.redirect = redirect
         self.redirect_port = redirect_port
 
@@ -80,7 +84,10 @@ class FilterRule(object):
             pass
         elif name == 'action':
             assert value in ('pass', 'block')
-        elif name in ('from_cidr', 'to_cidr', 'redirect'):
+        elif name in ('source', 'desination'):
+            if '/' in value:
+                value = netaddr.IPNetwork(value)
+        elif name == 'redirect':
             value = netaddr.IPNetwork(value)
         elif name.endswith('_port'):
             value = int(value)
@@ -88,12 +95,60 @@ class FilterRule(object):
             assert value in ('inet', 'inet6')
         elif name == 'protocol':
             assert value in ('tcp', 'udp', 'imcp')
+        elif name == 'redirect':
+            value = netaddr.IPAddress(redirect_port)
 
         super(FilterRule, self).__setattribute__(name, value)
+
+    @property
+    def pf_rule(self):
+        retval = [self.action]
+        if self.interface:
+            retval.append('on %s' % self.interface)
+        if self.family:
+            retval.append(self.family)
+        if self.protocol:
+            retval.append('proto %s' % self.protocol)
+        if self.source or self.source_port:
+            retval.append('from')
+            if self.source:
+                retval.append(str(self.source))
+            if self.source_port:
+                retval.append('port %s' % self.source)
+        if (self.destination_interface or self.destination or self.destinaton_port):
+            retval.append('to')
+            if self.destination_interface:
+                retval.append(self.destination_interface)
+            if self.destination:
+                retval.append(str(self.destination))
+            if self.destination_port:
+                retval.append('port %s' % self.destination_port)
+        if self.redirect:
+            retval.append('rdr-to')
+            retval.append(str(self.redirect))
+            retval.append('port %s' % self.redirect_port)
+        return  ' '.join(retval)
 
     @classmethod
     def from_dict(self, d):
         return FilterRule(**d)
+
+
+class Anchor(object):
+    def __init__(self, name, rules=[]):
+        self.name = name
+        self.rules = rules
+
+    @property
+    def pf_rule(self):
+        pf_rules = '\n\t'.join([r.pf_rule for r in self.rule])
+        return "anchor %s {\n%s\n}\n" % (self.name, pf_rules)
+
+    def external_pf_rule(self, base_dir):
+        path = os.path.abspath(os.path.join(base_dir, self.name))
+        return 'anchor %s\nload anchor %s from %s' % (self.name,
+                                                      self.name,
+                                                      path)
 
 
 class AddressBookEntry(object):
