ed69149ee927ddde674228a43240ed3d61ff2668
Duncan McGreggor <duncan@dreamhost.com>
2012-08-06 17:48:00 -0700

    Renamed _sudo back to sudo.

--- akanda/routerapi/drivers/base.py
+++ akanda/routerapi/drivers/base.py
@@ -5,7 +5,7 @@ class Manager(object):
     def __init__(self, root_helper='sudo'):
         self.root_helper = root_helper
 
-    def _sudo(self, *args):
+    def sudo(self, *args):
         return utils.execute([self.EXECUTABLE] + list(args), self.root_helper)
 
     def do(self, *args):
