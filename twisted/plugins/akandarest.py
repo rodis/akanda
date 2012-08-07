6b335a8033dfa8afc46aaf5d24a9543a4e5bbbf1
Duncan McGreggor <duncan@dreamhost.com>
2012-08-06 17:06:34 -0700

    Removed twisted plugin, now that we're switching to Flask.

--- twisted/plugins/akandarest.py
+++
@@ -1,8 +0,0 @@
-from twisted.application.service import ServiceMaker
-
-
-AkandaRESTAService = ServiceMaker(
-    "Akanda REST Server",
-    "akanda.service",
-    "DreamHost REST API Server for Akanda/Layer 3 Support",
-    "akanda")
