0183e9c34a9735418729466e70f73ae75b9594b4
Murali Raju <murali.raju@dreamhost.com>
2012-08-08 15:10:35 -0400

    Made devserver as the app server with v1 ask the blueprint for all routes. General clean up of pep8 violations

--- akanda/routerapi/devserver.py
+++ akanda/routerapi/devserver.py
@@ -1,10 +1,19 @@
-from akanda.routerapi.v1 import app
+import flask
+from akanda.routerapi import v1
+
+api = flask.Flask(__name__)
+api.register_blueprint(v1.blueprint, url_prefix='/v1')
+
+
+@api.before_request
+def attach_config():
+    pass
 
 
 def __main__():
-    app.debug = True
-    app.logger.debug('api_debug')
-    app.run(host='0.0.0.0', port=5000)
+    api.debug = True
+    #api.logger.debug('api_debug')
+    api.run(host='0.0.0.0', port=5000)
 
 
 if __name__ == "__main__":
