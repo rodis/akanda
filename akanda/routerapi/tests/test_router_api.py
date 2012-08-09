c7d99c55448b61fd083d52667a605e45e1f03c41
Murali Raju <murali.raju@dreamhost.com>
2012-08-09 14:06:45 -0400

    Added json as response type for anything using to_dict() in v1.py.

--- akanda/routerapi/tests/test_router_api.py
+++ akanda/routerapi/tests/test_router_api.py
@@ -3,9 +3,8 @@
 """
 
 import json
-import logging
 import unittest
-
+import logging
 import flask
 
 from akanda.routerapi import v1
@@ -13,25 +12,57 @@ from akanda.routerapi import v1
 LOG = logging.getLogger(__name__)
 
 
-class TestBase(unittest.TestCase):
+class TestRouterAPI(unittest.TestCase):
 
     def setUp(self):
-        super(TestBase, self).setUp()
+        super(TestRouterAPI, self).setUp()
         self.app = flask.Flask('test')
         self.app.register_blueprint(v1.blueprint)
         self.test_app = self.app.test_client()
 
         @self.app.before_request
-        def attach_storage_connection():
+        def attach_config():
+            '''
+               Add any config if needed
+            '''
             pass
 
     def tearDown(self):
         pass
 
-    def get(self, path):
-        rv = self.test_app.get(path)
+    def test_root(self):
+        rv = self.test_app.get('/v1/')
+        try:
+            data = rv.data
+        except ValueError:
+            print 'RAW DATA:', rv
+            raise
+        return data
+
+    def test_system_interfaces(self):
+        rv = self.test_app.get('/v1/system/interfaces')
         try:
             data = json.loads(rv.data)
+        except ValueError:
+            print 'RAW DATA:', rv
+            raise
+        return data
+
+    def test_system_interface(self):
+        rv = self.test_app.get('/v1/system/interface/ge1')
+        try:
+            data = json.loads(rv.data)
+            data = rv.data
+        except ValueError:
+            print 'RAW DATA:', rv
+            raise
+        return data
+
+    def test_firewall_rules(self):
+        rv = self.test_app.get('/v1/firewall/rules')
+        try:
+            #data = json.loads(rv.data)
+            data = rv.data
         except ValueError:
             print 'RAW DATA:', rv
             raise
