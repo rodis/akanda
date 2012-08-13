9715c73dfcf18b9e24167fb6a3f9bb03a7ba4b77
Murali Raju <murali.raju@dreamhost.com>
2012-08-13 16:01:22 -0400

    Instantiating if_mgr and pf_mgr per GET vs global similar to twisted

--- akanda/routerapi/v1.py
+++ akanda/routerapi/v1.py
@@ -37,8 +37,6 @@ def welcome():
 
 ## APIs for working with OpenBSD System.
 
-if_mgr = ifconfig.InterfaceManager()
-
 
 @blueprint.route('/system/interface/<ifname>')
 def get_interface(ifname):
@@ -46,6 +44,7 @@ def get_interface(ifname):
         Show interface parameters given an interface name.
         For example ge1, ge2 for generic ethernet
     '''
+    if_mgr = ifconfig.InterfaceManager()
     result = if_mgr.get_interface(ifname)
     js = json.dumps({"interface": result.to_dict()}, cls=utils.ModelSerializer)
     resp = Response(js, status=200, mimetype='application/json')
@@ -57,6 +56,7 @@ def get_interfaces():
     '''
         Show all interfaces and parameters
     '''
+    if_mgr = ifconfig.InterfaceManager()
     results = if_mgr.get_interfaces()
     interfaces = [x.to_dict() for x in results]
     js = json.dumps({"interfaces": interfaces}, cls=utils.ModelSerializer)
@@ -66,14 +66,13 @@ def get_interfaces():
 
 ## APIs for working with the Firewall.
 
-pf_mgr = pf.PfManager()
-
 
 @blueprint.route('/firewall/rules')
 def get_rules():
     '''
         Show loaded firewall rules by pfctl
     '''
+    pf_mgr = pf.PfManager()
     results = pf_mgr.get_rules()
     return results
 
@@ -83,6 +82,7 @@ def get_states():
     '''
         Show firewall state table
     '''
+    pf_mgr = pf.PfManager()
     results = pf_mgr.get_states()
     return results
 
@@ -92,6 +92,7 @@ def get_anchors():
     '''
         Show loaded firewall anchors by pfctl
     '''
+    pf_mgr = pf.PfManager()
     results = pf_mgr.get_anchors()
     return results
 
@@ -101,6 +102,7 @@ def get_sources():
     '''
         Show loaded firewall sources by pfctl
     '''
+    pf_mgr = pf.PfManager()
     results = pf_mgr.get_sources()
     return results
 
@@ -110,6 +112,7 @@ def get_info():
     '''
         Show verbose running firewall information
     '''
+    pf_mgr = pf.PfManager()
     results = pf_mgr.get_info()
     return results
 
@@ -119,6 +122,7 @@ def get_tables():
     '''
         Show loaded firewall tables by pfctl
     '''
+    pf_mgr = pf.PfManager()
     results = pf_mgr.get_tables()
     return results
 
@@ -128,6 +132,7 @@ def get_labels():
     '''
         Show loaded firewall labels by pfctl
     '''
+    pf_mgr = pf.PfManager()
     results = pf_mgr.get_labels()
     return results
 
@@ -137,6 +142,7 @@ def get_timeouts():
     '''
         Show firewall connection timeouts
     '''
+    pf_mgr = pf.PfManager()
     results = pf_mgr.get_timeouts()
     return results
 
@@ -146,5 +152,6 @@ def get_memory():
     '''
         Show firewall memory
     '''
+    pf_mgr = pf.PfManager()
     results = pf_mgr.get_memory()
     return results
