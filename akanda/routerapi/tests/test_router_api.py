9593664f9ce49dee7fcd17165e92db656059fc14
Duncan McGreggor <duncan@dreamhost.com>
2012-08-14 16:46:48 -0700

    Fixed up the fake interface manager to use model objects.

--- akanda/routerapi/tests/test_router_api.py
+++ akanda/routerapi/tests/test_router_api.py
@@ -10,19 +10,31 @@ from mock import patch
 
 from akanda import models
 from akanda.routerapi import v1
-from akanda.routerapi.drivers.ifconfig import InterfaceManager
+from akanda.routerapi.drivers.ifconfig import InterfaceManager as IFManager
 from akanda.routerapi.tests import payloads
 
 
-def fake_get_interface(self, ifname):
-    iface_data = json.loads(payloads.sample_system_interface)
-    return models.Interface.from_dict(iface_data["interface"])
+class FakeIFManager(object):
+    """
+    The methods implemented here in the fake interface manager should not be
+    built using the payloads, since that's what we're using to verify the data.
+    Instead, each method should create akanda objects as needed that will
+    serialize to the appropriate data to return the proper payload.
+    """
+    @classmethod
+    def fake_get_interface(cls, ifname):
+        return models.Interface(
+            media="Ethernet autoselect (1000baseT full-duplex,master)",
+            state="up",
+            ifname="ge1",
+            groups="egress",
+            lladdr="00:0c:29:e8:f9:2e",
+            addresses=["fe80::20c:29ff:fee8:f92e/64", "192.168.229.129/24"])
 
-
-def fake_get_interfaces(self):
-    # XXX this needs to be updated to load the data as json, and return a list
-    # of interface objects.
-    return payloads.sample_system_interfaces
+    @classmethod
+    def fake_get_interfaces(cls):
+        # XXX this needs to be updated to return a list of interface objects.
+        return payloads.sample_system_interfaces
 
 
 class BaseTestCase(unittest.TestCase):
@@ -53,12 +65,13 @@ class SystemTestCase(unittest.TestCase):
         self.app.register_blueprint(v1.system.system)
         self.test_app = self.app.test_client()
 
-    @patch.object(InterfaceManager, 'get_interface', fake_get_interface)
+    @patch.object(IFManager, 'get_interface', FakeIFManager.fake_get_interface)
     def test_get_interface(self):
         rv = self.test_app.get('/v1/system/interface/ge1')
         expected = payloads.sample_system_interface
         self.assertEqual(rv.data, expected)
 
+    # XXX add a patch decorator here
     def test_get_interfaces(self):
         rv = self.test_app.get('/v1/system/interfaces')
         expected = payloads.sample_system_interfaces
