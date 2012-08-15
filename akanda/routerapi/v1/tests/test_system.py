c52a520ccd6fd1ca6fa98d25234f88ee4af6b543
Duncan McGreggor <duncan@dreamhost.com>
2012-08-15 20:16:43 +0000

    Condensed code (removed indents).

--- akanda/routerapi/v1/tests/test_system.py
+++ akanda/routerapi/v1/tests/test_system.py
@@ -30,18 +30,20 @@ class FakeIFManager(object):
 
     @classmethod
     def fake_get_interfaces(cls):
-        interfaces = [models.Interface(media="null", state="down", ifname="ge0",
-                                       groups="enc", lladdr="null", addresses=[]),
-
-                      models.Interface(media="Ethernet autoselect (1000baseT full-duplex,master)",
-                                       state="up", ifname="ge1", groups="egress",
-                                       lladdr="00:0c:29:e8:f9:2e", 
-                                       addresses=["fe80::20c:29ff:fee8:f92e/64", "192.168.229.129/24"]),
-
-                      models.Interface(media="Ethernet autoselect (1000baseT full-duplex,master)",
-                                       state="up", ifname="ge2", groups= [], lladdr="00:0c:29:e8:f9:38",
-                                       addresses=["192.168.57.101/24", "fe80::20c:29ff:fee8:f938/64"]) ]
-        return interfaces
+        iface1 = models.Interface(
+            media="null", state="down", ifname="ge0", groups="enc",
+            lladdr="null", addresses=[])
+        iface2 = models.Interface(
+            media="Ethernet autoselect (1000baseT full-duplex,master)",
+            state="up", ifname="ge1", groups="egress",
+            lladdr="00:0c:29:e8:f9:2e", 
+            addresses=["fe80::20c:29ff:fee8:f92e/64", "192.168.229.129/24"])
+        iface3 = models.Interface(
+            media="Ethernet autoselect (1000baseT full-duplex,master)",
+            state="up", ifname="ge2", groups= [],
+            lladdr="00:0c:29:e8:f9:38",
+            addresses=["192.168.57.101/24", "fe80::20c:29ff:fee8:f938/64"])
+        return [iface1, iface2, iface3]
 
 
 class SystemAPITestCase(UnitTestCase):
@@ -61,7 +63,6 @@ class SystemAPITestCase(UnitTestCase):
         expected = payloads.sample_system_interface
         self.assertEqual(rv.data, expected)
 
-    # XXX add a patch decorator here
     @patch.object(IFManager, 'get_interfaces', FakeIFManager.fake_get_interfaces)
     def test_get_interfaces(self):
         rv = self.test_app.get('/v1/system/interfaces')
