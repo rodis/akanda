0e15d0324e10dd8639789dc1697289efc9030d1a
Mark McClain <mark.mcclain@dreamhost.com>
2012-08-29 18:56:07 -0400

    Finish repo reorg
    Moved files into the correct component dir for Horizon and Quantum
    extensions.  Update module and class import paths to match the new
    organization format.

--- akanda/horizon/akanda/alias/forms/__init__.py
+++
@@ -1,6 +0,0 @@
-from akanda.horizon.akanda.alias.forms.ports import (
-    CreatePortAliasForm, EditPortAliasForm)
-from akanda.horizon.akanda.alias.forms.hosts import (
-    CreateHostAliasForm, EditHostAliasForm)
-from akanda.horizon.akanda.alias.forms.networks import (
-    CreateNetworkAliasForm, EditNetworkAliasForm)
